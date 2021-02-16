51f78a33a51909daf2d49539daf5469f6110d2f1
WillCAI2020 <CRC681225@163.com>
2021-02-16 14:01:51 +0800

    修改：调整评论自定义表情展示+友链懒加载

--- source/js/links.js
+++ source/js/links.js
@@ -1,3 +1,4 @@
+document.write("<script language='javascript' src='https://cdn.jsdelivr.net/npm/lazyload@2.0.0-rc.2/lazyload.js' ></script>");
 link1 = {
   init: function () {
     var that = this;
@@ -19,7 +20,7 @@ link1 = {
 	  description = data[i].description;
       li +=
         '<div class="blogroll">' + 
-        '<img class="avatar" src=" ' + avatar + ' ">' +
+        '<img class="lazyload avatar" src="https://cdn.jsdelivr.net/gh/WillCAI2020/cdn/images/loading.png" data-src=" ' + avatar + ' ">' +
 		'<a class="friend" href="' +
         site +
         '" target="_blank">' +
@@ -31,6 +32,7 @@ link1 = {
         '</div>';
     }
     $(".link-navigation_dalao").append(li);
+	$("img.lazyload").lazyload();
   },
 };
 link1.init();
@@ -56,7 +58,7 @@ link2 = {
 	  description = data[i].description;
       li +=
         '<div class="blogroll">' + 
-        '<img class="avatar" src=" ' + avatar + ' ">' +
+        '<img class="lazyload avatar" src="https://cdn.jsdelivr.net/gh/WillCAI2020/cdn/images/loading.png" data-src=" ' + avatar + ' ">' +
 		'<a class="friend" href="' +
         site +
         '" target="_blank">' +
@@ -68,6 +70,7 @@ link2 = {
         '</div>';
     }
     $(".link-navigation").append(li);
+	$("img.lazyload").lazyload();
   },
 };
 link2.init();
