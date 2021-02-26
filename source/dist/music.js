b111db2fd5aba015344f17e4978b08fccc33a35f
WillCAI2020 <CRC681225@163.com>
2021-02-27 02:11:34 +0800

    添加歌词

--- source/dist/music.js
+++ source/dist/music.js
@@ -2,18 +2,21 @@ const ap = new APlayer({
     container: document.getElementById('aplayer'),
 	fixed: true,
 	autoplay: true,
+	lrcType: 3,
     audio: [
 		{
 			name: '慵懒的猫',
 			artist: 'Hea2t',
 			url: 'https://cdn.jsdelivr.net/gh/WillCAI2020/cdn/music/慵懒的猫.mp3',
-			cover: 'http://p1.music.126.net/89f0ghqzPenQCwwxki4_fw==/109951164594173915.jpg?param=130y130'
+			cover: 'http://p1.music.126.net/89f0ghqzPenQCwwxki4_fw==/109951164594173915.jpg?param=130y130',
+			lrc: '/dist/Hea2t.lrc'
 		},
 		{
 			name: '飞鸟与蝉',
 			artist: '可可',
-			url: 'http://m10.music.126.net/20210227014636/d9cc07bee3cb0c40e76e9a11eee638f1/ymusic/obj/w5zDlMODwrDDiGjCn8Ky/3804202104/c2aa/fcec/3724/a31684e04242eb1538f1ef75b900624b.mp3',
-			cover: 'http://p2.music.126.net/4pTCC_lBDmoiti8lxOu8_Q==/109951165277363176.jpg?param=130y130'
+			url: 'https://willcai2020.gitee.io/files/music/%E9%A3%9E%E9%B8%9F%E4%B8%8E%E8%9D%89.mp3',
+			cover: 'http://p2.music.126.net/4pTCC_lBDmoiti8lxOu8_Q==/109951165277363176.jpg?param=130y130',
+			lrc: '/dist/飞鸟和蝉-可可.lrc'
 		}
 	]
 });
